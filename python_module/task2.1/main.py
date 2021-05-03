2d8d918f95fb224974086f6ac71fe2a6ae75b276
Bogdan Artem <bogdan_artem@icloud.com>
2021-05-03 11:00:04 +0300

    main.py Refactored

--- python_module/task2.1/main.py
+++ python_module/task2.1/main.py
@@ -2,65 +2,105 @@ import tkinter as tk
 from tkinter import messagebox
 from hangman import Hangman
 
-root = tk.Tk()
-new_game = Hangman(5)
+class Application(tk.Frame):            
+    def __init__(self, master=None):
+        tk.Frame.__init__(self, master)  
+        self.grid()                      
+        self.game = Hangman()
+        self.create_widgets()
 
+    def create_widgets(self):
+        """Create all widgets and display them."""
+        self._add_lives(row=0, column=0)
+        self._add_display_hangman(row=1, column=0)
+        self._add_text_input(row=2, column=0)
+        self._add_button(row=3, column=0)
+        self._add_attemps(row=4, column=0)
+        
+    def click(self):
+        """Click initialized by "guess button.""""
+        # Get user input
+        self.user_inp = self.text_input.get().lower()
+        self.game.guess(self.user_inp)
+        self.main_text.config(text=self.game.get_masked_word())
 
-def click():
-    user_inp = main_input.get().lower()
-    print(user_inp)
-    new_game.guess(user_inp)
-    print(new_game.get_masked_word())
-    main_text.config(text=new_game.get_masked_word())
+        self._click_update_attempts()
+        self._click_update_lives()
+        self._click_if_endgame()
 
-    # Add text to "display" label
-    label_text = attempts_var.get()
-    label_text += user_inp + " "
-    attempts_var.set(label_text)
+    def _click_update_attempts(self):
+        """Update displayed attempts."""
+        label_text = self.attempts_var.get()
+        label_text += self.user_inp + " "
+        self.attempts_var.set(label_text)
 
-    # Change lives count
-    label_num = lives_var.get()
-    label_num = f"Lives: {new_game.lives}"
-    lives_var.set(label_num)
+    def _click_update_lives(self):
+        """Update displayed lives."""
+        label_num = self.lives_var.get()
+        label_num = f"Lives: {self.game.get_lives()}"
+        self.lives_var.set(label_num)
 
-    if new_game.is_guessed():
-        messagebox.showinfo("CONGRATS!", "You nailed it!")
-        root.quit()
+    def _click_if_endgame(self):
+        """ Checks if the game has ended """
+        if self.game.is_guessed():
+            messagebox.showinfo("CONGRATS!", "You nailed it!")
+            self.quit()
 
-    elif new_game.is_dead():
-        messagebox.showinfo(
-            "WASTED!", 
-            f"You've run out of lives. The word was {new_game.get_word()}"
-            )
-        root.quit()
-    
+        elif self.game.is_dead():
+            messagebox.showinfo(
+                "WASTED!", 
+                f"You've run out of lives. The word was {self.game.get_word()}"
+                )
+            self.quit()
 
-main_text = tk.Label(root, text=new_game.get_masked_word(), font=("Kumar One", 50))
-tried = tk.Label(root, text="Attempted:")
-main_input = tk.Entry(root, justify='center')
-guess_button = tk.Button(root, text='Enter', command=click, width=21, highlightcolor='blue')
+    def _add_lives(self, row, column):
+        """Add widget for displaying lives"""
+        # Create widget variable for future change
+        self.lives_var = tk.StringVar()
+        # Get widget variable value and change it
+        label_num = self.lives_var.get()
+        label_num = f"Lives: {self.game.lives}"
+        # Lock widget variable
+        self.lives_var.set(label_num)
+        self.lives = tk.Label(self, textvariable=self.lives_var)    
+        # Display widget variable
+        self.lives.grid(row=0, column=0)
+          
+    def _add_attemps(self, row, column):
+        """Add widget for displaying attempts"""
+        # Create widget variable for future change
+        self.attempts_var = tk.StringVar()
+        # Get widget variable value and change it
+        label_text = self.attempts_var.get()
+        label_text += "Attempts:\n"
+        # Lock widget variable
+        self.attempts_var.set(label_text)
+        self.attempts = tk.Label(self, textvariable=self.attempts_var)
+        # Display widget variable
+        self.attempts.grid(row=row, column=column)
 
-# Show lives
-lives_var = tk.StringVar()
-label_num = lives_var.get()
-label_num = f"Lives: {new_game.lives}"
-lives_var.set(label_num)
-lives = tk.Label(root, textvariable=lives_var)
+    def _add_display_hangman(self, row, column):
+        """Add widget for displaying words of hangman"""
+        self.main_text = tk.Label(self, text=self.game.get_masked_word(), font=("Kumar One", 50))
+        self.main_text.grid(row=row, column=column)
 
+    def _add_text_input(self, row, column):
+        """Add widget for inputing letters or words"""
+        self.text_input = tk.Entry(self, justify='center')
+        self.text_input.grid(row=row, column=column)
 
-# Show attempts
-attempts_var = tk.StringVar()
-label_text = attempts_var.get()
-label_text += "Attempts:\n"
-attempts_var.set(label_text)
-attempts = tk.Label(root, textvariable=attempts_var)
+    def _add_button(self, row, column):
+        """Add button to initialize "click" function"""
+        self.guess_button = tk.Button(
+            self, 
+            text='Enter', 
+            command=self.click, 
+            width=21, 
+            highlightcolor='blue'
+        )
+        self.guess_button.grid(row=row, column=column)
+        
 
-
-main_text.grid(row=1, column=0)
-main_input.grid(row=2, column=0)
-guess_button.grid(row=3, column=0)
-
-attempts.grid(row=4, column=0)
-lives.grid(row=0, column=0)
-
-root.mainloop()
+app = Application()                      
+app.master.title('Hangman')  
+app.mainloop()  
