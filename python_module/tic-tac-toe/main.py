5587a073140d9d23e4aaa347649a42df16fd45ed
Bogdan Artem <bogdan_artem@icloud.com>
2021-05-24 14:51:53 +0300

    Changed diagonal check, added CLI UI

--- python_module/tic-tac-toe/main.py
+++ python_module/tic-tac-toe/main.py
@@ -2,104 +2,112 @@
 
 
 import logging
-from typing import Tuple
 from collections import deque
+
+
 logging.basicConfig(level='INFO')
 
 
-class Board:
+class Game:
+    """Class that represents game with players"""
+
     def __init__(self, size=3, streak_size=3):
         self.streak_size = streak_size
-        self.board = [['*'] * size for _ in range(size)]
-        self.size = size
+        self.board_size = size
+        self.board = [[' '] * size for _ in range(size)]
         self.symbols = ['X', 'O']
         self.game_over = False
         self.turns = 1
 
-    def __str__(self):
+    def __repr__(self):
+        """Game representation.
+        
+        Row consists of 3 textual lines. Contiguous rows share 1 textual line"""
+        picture = ""
         for row in self.board:
-            return str(row)
+            line1 = "".join([" -- " for _ in row]) + "\n"
+            line2 = "".join(["| " + element + " " for element in row]) + "|\n"
+            picture += (line1 + line2)
+        picture += "".join([" -- " for _ in row]) + "\n"
+        return picture
 
     def play(self):
+        """Start game and receive input."""
         while not self.game_over:
-            x, y = [int(x) for x in input("Your move: ").split()]
-            self.move(x, y)
+            print(self)
+            x_point, y_point = [int(x) for x in input("Your move: ").split()]
+            self.move(x_point, y_point)
 
-    def move(self, x, y):
+    def move(self, x_point, y_point):
+        """Change symbols on board."""
         symbol = self.symbols[self.turns//2]
-        logging.info(f"Player {symbol} is moving")
-        logging.info(f"Making move at x:{x}, y:{y}...")
-        self.board[x][y] = symbol
-        logging.info(self)
-        # self.turns += 1
-        logging.info(f"Turn number: {self.turns}")
-        self._check_in_all_dimensions()
-        logging.info(f"Game over is: {self.game_over}")
+        logging.info("Player %s is moving", symbol)
+        logging.debug("Making move at x:%d, y:%d...", x_point, y_point)
+        self.board[x_point][y_point] = symbol
 
+        # self.turns += 1
+        logging.info("Turn number: %d", self.turns)
+        self._check_in_all_dimensions()
+        logging.info("Game over is: %")
 
     def _check_in_all_dimensions(self):
+        """Check for endgame in rows and diagonals.
+
+        After check of all rows and diagonals the board is
+        Turned at 90 degrees for another check
+        """
         # Copy for board rotation
         board_copy = self.board.copy()
+        for _ in range(2):
+            self._check_rows(board_copy)
+            self._check_diagonals(board_copy)
+            # Rotate board clockwise
+            board_copy = list(zip(*board_copy[::-1]))
 
-        self._check_rows(board_copy)
-        self._check_major_diagonal(board_copy)
-        self._check_minor_diagonals(board_copy)
-        # Rotate board clockwise
-        board_copy = list(zip(*board_copy[::-1]))
+    def _check_diagonals(self, board):
+        """Traverse matrix diagonally.
 
-        self._check_rows(board_copy)
-        self._check_major_diagonal(board_copy)
-        self._check_minor_diagonals(board_copy)
-
-        board_copy = list(zip(*board_copy[::-1]))
-        self._check_minor_diagonals(board_copy)
-
-        board_copy = list(zip(*board_copy[::-1]))
-        self._check_minor_diagonals(board_copy)
-
+        Link to the algorithm https://www.youtube.com/watch?v=T8ErAYobcbc
+        """
+        row = len(board)
+        col = len(board[0])
+        for line in range(1, (row + col)):
+            start_col = max(0, line - row)
+            count = min(line, (col - start_col), row)
+            diagonal_queue = deque(maxlen=self.streak_size)
+            for j in range(0, count):
+                diagonal_queue.append(board[min(row, line) - j - 1][start_col + j])
+                if len(diagonal_queue) == self.streak_size and not self.game_over:
+                    self._check_game_over(diagonal_queue)
 
     def _check_rows(self, board):
+        """"Traverse all rows and find streak of 'X' or 'O' """
         for row in board:
             # Reset queue for each row
-            winning_queue = deque(maxlen=self.streak_size)
+            row_queue = deque(maxlen=self.streak_size)
             for element in row:
-                winning_queue.append(element)
-                if len(winning_queue) == self.streak_size and not self.game_over:
-                    self._check_game_over(winning_queue)
-
-    def _check_minor_diagonals(self, board):
-        """Check small diagonals in one corner of the board
-        
-        To check all 4 corners the board has to be rotated"""
-        pass
-
-    def _check_major_diagonal(self, board):
-        """Check the largest diagonal (left-top to bottom-right) for endgame"""
-        sequence = (board[x][x] for x in range(len(board)))
-        winning_queue = deque(maxlen=self.streak_size)
-        for element in sequence:
-            #TODO: don't check if less that streak size
-            if not self.game_over: 
-                winning_queue.append(element)
-                self._check_game_over(winning_queue)
+                row_queue.append(element)
+                if len(row_queue) == self.streak_size and not self.game_over:
+                    self._check_game_over(row_queue)
 
     def _check_game_over(self, sequence):
         """Checks if one of sides has won"""
         win1 = [self.symbols[0]] * self.streak_size
         win2 = [self.symbols[1]] * self.streak_size
-        final = list(sequence)
-        if final == win1:
+        sequence = list(sequence)
+        if sequence == win1 or sequence == win2:
             self.game_over = True
-        elif final == win2:
-            self.game_over = True
-
 
 
 def main():
-    game = Board(size=4, streak_size=3)
-    game.play()
+    """Entrypoint for the game"""
+    game = Game(size=3, streak_size=3)
+    player1 = Player("Dima")
+    player2 = Player("Vasya")
 
-    
+    while True:
+        player1.make_move(game)
+        player2.make_move(game)
 
 
 if __name__ == '__main__':
