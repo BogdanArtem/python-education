89d4f82cc34b839f1743fd393586e2fd8c3c69c9
Bogdan Artem <bogdan_artem@icloud.com>
2021-05-26 21:49:01 +0300

    Added dynamic array

---
+++ algorithms/data_structures/test_dynamic_array.py
@@ -0,0 +1,33 @@
+"""Binary tree tests"""
+
+import pytest
+from dynamic_array import DynamicArray
+
+def test_dynamic_array():
+    arr = DynamicArray(3)   
+
+    arr[0] = "this"
+    arr[1] = "is"
+    arr[2] = "just"
+    arr[3] = "another"
+    arr[4] = "test"
+    arr[5] = "!"
+
+    assert arr.num_items == 6
+    assert arr.size == 12
+
+    arr[0] = None
+    assert arr.num_items == 5
+
+    arr[1] = "text"
+    assert arr.num_items == 5
+
+
+
+
+
+
+
+
+
+    
