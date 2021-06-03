44db8f86a3ca1f4c5c6b3787b78f483572e2d414
Bogdan Artem <bogdan_artem@icloud.com>
2021-06-03 16:57:30 +0300

    Reworked main menu

--- python_module/tic-tac-toe/main.py
+++ python_module/tic-tac-toe/main.py
@@ -1,64 +1,53 @@
 """Tic-Tac-Toe game with arbitrary size of board and size of streak"""
 
+
 import sys
 from player import Player
 from minimax import AI
 from game import Game
 
-player1 = Player(name="Default1", sign="X")
-player2 = Player(name="Default2", sign="O")
-ai = AI(name="AI", sign="O")
 
-
-def get_player(num):
+def get_player():
     """Get user input"""
     p_inp = False
     while not p_inp:
         try:
-            name, sign = input(f"Enter the name of player{num} \
-                and it's sign, e.g. 'Alex X': ").split()
+            name, sign = input("Enter the \
+player name and it's sign, e.g. 'Alex X': ").split()
+            is_ai = input("Is it AI? Y/N: ").lower()
             if len(sign) > 1:
                 raise ValueError
+            if is_ai not in ("y", "n"):
+                raise ValueError
             p_inp = True
+            is_ai = True if is_ai == "y" else False
         except ValueError:
-            print("Please, enter valid name and sign")
-    return name, sign
+            print("Please, enter valid data")
+    return AI(name, sign) if is_ai else Player(name, sign)
 
 
 def get_game():
-    """Get user input"""
+    """Get user input for game"""
     p_inp = False
     while not p_inp:
         try:
-            size, streak = [int(x) for x in input(f"Enter the size of the board and streak size like '5 3': ").split()]
+            size, streak = [int(x) for x in input("Enter the size \
+of the board and streak size like '5 3': ").split()]
             if streak > size:
                 raise ValueError
             p_inp = True
         except ValueError:
             print("Please, enter valid name and sign")
-    return size, streak
+    return Game(size, streak)
 
 
-def custom_players():
-    """Change default players"""
-
-    name1, sign1 = get_player(1)
-    name2, sign2 = get_player(2)
-
-    global player1
-    global player2
-    player1 = Player(name1, sign1)
-    player2 = Player(name2, sign2)
-    print("Names changed \n")
-    main()
-
-
-def new_game(player):
+def new_game():
     """Start new game with adaptive size of board"""
-    size, streak_size = get_game()
-    game = Game(size=size, streak_size=streak_size)
+    game = get_game()
+    player1 = get_player()
+    player2 = get_player()
     game.add_player(player1)
-    game.add_player(player)
+    game.add_player(player2)
     game.start()
 
 
@@ -83,24 +72,18 @@ def main():
     """Entrypoint for the game"""
 
     print("1 - New game")
-    print("2 - New game with AI")
-    print("3 - Custom names")
-    print("4 - Check logs")
-    print("5 - Delete logs")
-    print("6 - Exit")
+    print("2 - Check logs")
+    print("3 - Delete logs")
+    print("4 - Exit")
 
     num = int(input('\n-> '))
     if num == 1:
-        new_game(player2)
+        new_game()
     elif num == 2:
-        new_game(ai)
-    elif num == 3:
-        custom_players()
-    elif num == 4:
         check_logs()
-    elif num == 5:
+    elif num == 3:
         delete_logs()
-    elif num == 6:
+    elif num == 4:
         sys.exit()
     else:
         print("Wrong number!")
