c76d2c546a34517d8a6f572e98883c78df62d7fa
Артем Богдан <bogdan_artem@icloud.com>
2021-06-09 17:05:06 +0300

    queries/task2.2

--- databases/simple_queries/get_data.sql
+++ databases/simple_queries/get_data.sql
@@ -8,3 +8,6 @@ select * from products;
 /* Select all order status */
 select * from order_status;
 
+/* Task 2.2 */
+/* Select paid and delivered orders */
+select * from orders where order_status_order_status_id = 4;
