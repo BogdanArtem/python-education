2ba39700701bbf00ddf37313484834baf7b6cb04
Артем Богдан <bogdan_artem@icloud.com>
2021-06-10 22:05:40 +0300

    queries/task3.1

---
+++ databases/simple_queries/queries2.sql
@@ -0,0 +1,20 @@
+/* Task 3.1*/
+create table if not exists potential_customers(
+	id integer primary key,
+	email varchar(90),
+	name varchar(50),
+	surname varchar(90),
+	second_name varchar(90),
+	city varchar(90)
+);
+
+/* Select from potential customers and customers form city 17  */
+select first_name, email 
+from
+	(select user_id, email, first_name, middle_name, last_name, city 
+	from users 
+	union
+	select id, email, name, surname, second_name, city
+	from potential_customers pc) as everyone
+where city = 'city 17'
+
